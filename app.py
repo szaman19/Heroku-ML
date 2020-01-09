040929fe6770c67d9995cc26eadc4981fd5272da
Shehtab Zaman <shehtabzaman@gmail.com>
2020-01-09 15:00:02 -0500

    Canvas upload mechanism completed

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+import io
+import json 
+
+# from torchvision import models
+# import torchvision.transforms  as transforms
+from PIL import Image
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "Rachel is Crazy!"
+def hello():
+	return "Hello"
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+	if(request.method == "POST"):
+		file = request.files['Image']
+		# print(file)
+		img_bytes = file.read()
+		image = Image.open(io.BytesIO(img_bytes))
+		image.save("test.png")
+
+		response = jsonify({'body': 'data'})
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		return response
