8a7b1afb0dc0af4d28518de8136918360a406f71
Shehtab Zaman <shehtabzaman@gmail.com>
2020-01-10 16:15:59 -0500

    Completed vanilla neural network

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import io
 import json 
+import torch 
 
 # from torchvision import models
 import torchvision.transforms  as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
 import numpy as np 
+import model
 
 app = Flask(__name__)
 
@@ -13,24 +15,64 @@ app = Flask(__name__)
 def hello():
 	return "Hello"
 
+def prediction(img_bytes):
+	tensor = transform_image(img_bytes)
+	device = torch.device("cpu")
+	m = model.Net()
+	m.load_state_dict(torch.load("mnist_cnn.pt", map_location=torch.device('cpu')))
+	m.eval()
+
+	with torch.no_grad():
+		tensor = tensor.to(device)
+		outputs = m.forward(tensor)
+		# print(outputs)
+		y_hat = outputs.argmax(dim=1, keepdim = True)
+
+	return y_hat.item()
+
+
+
+
+
 
 def transform_image(img_bytes):
 	transformations = transforms.Compose([transforms.Grayscale(), 
 										  transforms.Resize(28)])
 	image = Image.open(io.BytesIO(img_bytes)).convert('RGBA')
 	image.save("pretransform.png")
+
 	x = np.array(image)
 	r, g, b, a = np.rollaxis(x, axis = -1)
 	r[a == 0] = 255
 	g[a == 0] = 255
 	b[a == 0] = 255
 	x = np.dstack([r, g, b, a])
+
 	img = Image.fromarray(x, 'RGBA')
-	# background = Image.new('RGBA', png.size, (255,255,255))
-	# img = Image.alpha_composite(background, Image)
-	# img.save("")
-	transformed_image = transformations(img)
-	transformed_image.save("test_3.png")
+	
+	img = transformations(img)
+
+	img = img.convert('L')
+
+	a = np.array(image)
+	val = a.flatten()
+	mean = np.mean(val) / 255
+	std = np.std(val) / 255
+
+	trans = transforms.Compose([transforms.ToTensor(),
+		transforms.Normalize((mean,), (std,))])
+
+	tensor = trans(img)
+
+	re_trans = transforms.ToPILImage()
+
+	re_img = re_trans(tensor)
+	# re_img.save("transform_and_back_test.png")
+	# print(tensor.shape)
+	# transformed_image = transformations(img)
+	# transformed_image.save("test_3.png")
+
+	return tensor.unsqueeze(0)
 
 
 @app.route('/predict', methods = ['POST'])
@@ -39,9 +81,12 @@ def predict():
 		file = request.files['Image']
 		# print(file)
 		img_bytes = file.read()
-		transform_image(img_bytes)
+		predicted = prediction(img_bytes)
 		
+		print(predicted)
 
-		response = jsonify({'body': 'data'})
+		response = jsonify({'prediction': str(predicted)})
 		response.headers.add('Access-Control-Allow-Origin', '*')
+		response.headers.status = 200
+		response.headers.mimetype = 'application/json'
 		return response
