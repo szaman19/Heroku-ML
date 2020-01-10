ace1da1725600523de3a1f1468c7153230c8cf93
Shehtab Zaman <shehtabzaman@gmail.com>
2020-01-10 13:12:04 -0500

    Updated requirements.txt to remove pkg-resource

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import torchvision.transforms  as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
+import numpy as np 
 
 app = Flask(__name__)
 
@@ -14,10 +15,22 @@ def hello():
 
 
 def transform_image(img_bytes):
-	transformations = transforms.Compose([transforms.Resize(28)])
-	image = Image.open(io.BytesIO(img_bytes)).convert('LA')
-	transformed_image = transformations(image)
-	transformed_image.save("test.png")
+	transformations = transforms.Compose([transforms.Grayscale(), 
+										  transforms.Resize(28)])
+	image = Image.open(io.BytesIO(img_bytes)).convert('RGBA')
+	image.save("pretransform.png")
+	x = np.array(image)
+	r, g, b, a = np.rollaxis(x, axis = -1)
+	r[a == 0] = 255
+	g[a == 0] = 255
+	b[a == 0] = 255
+	x = np.dstack([r, g, b, a])
+	img = Image.fromarray(x, 'RGBA')
+	# background = Image.new('RGBA', png.size, (255,255,255))
+	# img = Image.alpha_composite(background, Image)
+	# img.save("")
+	transformed_image = transformations(img)
+	transformed_image.save("test_3.png")
 
 
 @app.route('/predict', methods = ['POST'])
--- model.py
+++ model.py
@@ -55,6 +55,7 @@ def test(model, device, test_loader):
 	    for data, target in test_loader:
 	        data, target = data.to(device), target.to(device)
 	        output = model(data)
+	        print(output)
 	        test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
 	        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
 	        correct += pred.eq(target.view_as(pred)).sum().item()
